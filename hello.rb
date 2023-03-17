85c76dcd450bead2d1bab34defe180d1d47cb622
Michana <michana3768@gmail.com>
2023-03-17 22:33:24 +0200

    3 commit

--- hello.rb
+++ hello.rb
@@ -1,4 +1,3 @@
-
 puts "Введите Ваше имя: "
  name = gets.chomp
 
@@ -7,27 +6,8 @@ puts "Введите Ваш рост:"
 
  perfect_weight = height - 110
 
- if perfect_weight < 0
+if perfect_weight < 0
 puts "Ваш вес уже оптимальный."
- else
-   puts "#{name}, Ваш идеальный вес: #{perfect_weight}"
- end
-
-#puts "Введите своё имя"
- #name = gets.chomp
-
- #puts "Какой у вас рост?"
- #name = gets.chomp
- #user_imput_in_height = gets.to_i
- #result_in_weight= user_imput_in_height  - 110
-
-  #if perfect_weight < 0
- #puts "Ваш вес уже оптимальный."
-  #else
- #puts "#{name}, Ваш идеальный вес: #{perfect_weight}"
-  #end
-
-
-
-
-
+else
+puts "#{name}, Ваш идеальный вес: #{perfect_weight}"
+end
